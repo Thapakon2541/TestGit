f0fafe590bcfd38ecf10907441e617238a2f9609
Thapakon <36725707+Thapakon2541@users.noreply.github.com>
2020-10-30 16:44:36 +0700

    Update Calculation.java

--- JAVA/Calculation.java
+++ JAVA/Calculation.java
@@ -24,6 +24,6 @@ public  class Calculation {
       demo.addition(a, b);
       demo.Subtraction(a, b);
       demo.multiplication(a, b);
-      demo.multiplication(b,a);
+      http://demo.traccar.org/api/users
    }
 }
