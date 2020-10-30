a0a85ee84749f8fd6f0d3ab6f8445320a62e46de
Thapakon <36725707+Thapakon2541@users.noreply.github.com>
2020-10-30 14:07:58 +0700

    Create Car.java

---
+++ JAVA/Car.java
@@ -0,0 +1,17 @@
+class Vehicle { /// inheritanc การถ่ายทอดคุณสมบัติ // Super
+    protected String brand = "ford";
+    protected int   speed = 300;
+    public void honk(){
+        System.out.println("TUST , TUST");
+
+    }
+}
+class Car extends Vehicle{ // Subclass
+    private String  modelname = "Mustang";
+    public static void main (String[] args){
+            Car myFastCar = new Car();
+            myFastCar.honk();
+            System.out.println(myFastCar.brand+"   "+myFastCar.modelname+"Speed:"+myFastCar.speed);
+    }
+
+}
