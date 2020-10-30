65ee711db8a172789463cf41be13c54546426954
Thapakon <36725707+Thapakon2541@users.noreply.github.com>
2020-10-30 14:07:48 +0700

    Create Myclass.java

---
+++ JAVA/Myclass.java
@@ -0,0 +1,7 @@
+class Myclass {
+    public static void main(String[] args) {
+        Person myObj = new Person();
+        myObj.setName("peen");
+        System.out.println(myObj.getName());
+    }
+}
