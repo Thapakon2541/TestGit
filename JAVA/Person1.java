8f017a47e141dc428424fa6935bb98026fa84f59
Thapakon <36725707+Thapakon2541@users.noreply.github.com>
2020-10-30 14:07:33 +0700

    Create Person1.java

---
+++ JAVA/Person1.java
@@ -0,0 +1,41 @@
+// public class  Person1{ //การนำ objไปใช้งาน 
+//     public static void main (String[] args){
+//         Person p1 = new Person();
+//         Person p2 = new Person();
+//         p1.name = "peen";
+//         p1.age = 20;
+//         p1.speed=3.5f;
+
+//         p2.name="name";
+//         p2.age=25;
+//         System.out.println(" p1 "+p1.name);
+//         System.out.println("He is age "+p1.age+"Yearold");
+//         System.out.println(p1.name+"Has speed"+p1.speed);
+//         p1.run();
+//         System.out.println(" p1 "+p2.name);
+//         System.out.println("He is age "+p2.age+"Yearold");
+//         p2.run();
+//         p2.setSpeed(3.5f);
+//     }
+// }
+
+//    class Person {
+//    public  String  name  ;
+//    public   int   age;
+//     float   speed = 4.0f;
+//     void  setSpeed (float newspeed){
+//         speed = newspeed;
+
+//     }
+//      float getSpeed() {
+//         return speed;
+//     }
+//     void run(){
+//         System.out.println(name + " is running at speed " + speed + " feet/s.");
+//     }
+// 	public void setName(String string) {
+// 	}
+// 	public char[] getName() {
+// 		return null;
+// 	}
+// }
